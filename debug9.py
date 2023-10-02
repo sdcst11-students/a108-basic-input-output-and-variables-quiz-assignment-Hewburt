54cb58980f2bc27c5e4d8a68fa31e79ad06f488d
Hewburt <hewittlacroix@gmail.com>
2023-10-02 14:20:32 -0700

    Update debug9.py

--- debug9.py
+++ debug9.py
@@ -2,7 +2,7 @@
 """
 Debug this program so that it runs
 """
-x = input("Enter a number")
+x = input("Enter a number: ")
 y = 2
-answer = x+y
-print(The answer is + answer)
+answer = int(x) + y  
+print("The answer is", answer)
